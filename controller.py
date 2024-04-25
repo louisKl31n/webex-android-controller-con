83ea92a8b2455624e7cfad8c66218047c4ba6c71
Louis KLEIN <louis.klein@oange.com>
2024-04-25 17:57:10 +0200

    Updated : webex_log_in_bis

--- controller.py
+++ controller.py
@@ -371,6 +371,21 @@ class Controller:
         orange_portal_connect.click()
 
 
+        self.driver.execute_script('mobile: shell',{'command' : 'input keyevent 4'})
+        self.swipe_vertical(300)
+        orange_portal_next = self.find_by_XPATH_inside_parent(orange_portal_webview,'//android.widget.Button[@text="Suivant"]')
+        orange_portal_next.click()
+            # wait until the page loads to password view
+        self.wait_until_element_is_displayed('//android.view.View[@text="Saisissez votre mot de passe"]',10)
+        self.swipe_vertical(500)
+        orange_portal_webview = self.find_by_XPATH('//android.webkit.WebView[@text="Authentication B2B"]')
+        orange_portal_password = self.find_by_XPATH_inside_parent(orange_portal_webview,'//android.widget.EditText')
+        orange_portal_password.send_keys(password)
+            # in order to have the connection button visible we need swipe
+        orange_portal_connect = self.find_by_XPATH_inside_parent(orange_portal_webview,'//android.widget.Button[@text="Se connecter"]')
+        orange_portal_connect.click()
+
+
         # Check for a toasts from the application and accept it
         try :   
             self.wait_until_element_is_displayed('(//android.widget.ImageView[@resource-id="com.cisco.wx2.android:id/avatarBackground"])[1]',10)
