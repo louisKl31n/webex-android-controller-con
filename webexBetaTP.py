68673bb2ae33eaedc09bd6ba99414f78d9fbb32f
asignor <alan.signor.ext@orange.com>
2024-08-19 15:53:50 +0200

    feu de circulation

--- webexBetaTP.py
+++ webexBetaTP.py
@@ -2,6 +2,7 @@ from csv import writer
 import requests
 from datetime import datetime
 from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 import time
 
 web_server = "http://127.0.0.1:5000"
@@ -15,6 +16,8 @@ token2 = 0
 phoneNumberWebexBeta1 = '0789182612'
 phoneNumberWebexBeta2 = '0789182614'
 transfertTarget = '0789182689'
+redFill = PatternFill(start_color= "FF0000", end_color= "FF0000", fill_type="solid")
+greenFill = PatternFill(start_color= "00FF00", end_color= "00FF00", fill_type="solid")
 
 class Tests :
     def MNCQUALIF_10966_in(deviceName,email,token) :
@@ -99,7 +102,18 @@ class Tests :
             'deviceName': deviceName1,
             'token': token1,
             })
-        return(step1.status_code == 200)
+        time.sleep(5)
+        step2 = requests.post(web_server+'/answer', json={
+            'deviceName': deviceName2,
+            'token': token2,
+            'incomingNumber': phoneNumberWebexBeta1
+            })
+        time.sleep(5)
+        step3 = requests.post(web_server+'/hang-up', json={
+            'deviceName': device_name1,
+            'token': token1,
+            })
+        return(step1.status_code == 200 and step2.status_code == 200 and step3.status_code == 200)
 
     def MNCQUALIF_11009() :    
         step1 = requests.post(web_server+'/call', json={
@@ -150,7 +164,20 @@ class Tests :
         return (step1.status_code == 200 and step2.status_code == 200 and step3.status_code == 200 and step4.status_code == 200 and step5.status_code == 200)
 
     def MNCQUALIF_11012() :
-        step1 = requests.post(web_server+'/video-call', json={
+        step1 = requests.post(web_server+'/call', json={
+        'deviceName': deviceName1,
+        'token': token1,
+        'incomingNumber': phoneNumberWebexBeta2
+        })
+        time.sleep(5)
+        step2 = requests.post(web_server+'/answer', json={
+            'deviceName': deviceName2,
+            'token': token2,
+            'incomingNumber': phoneNumberWebexBeta1
+            })
+        time.sleep(5)
+
+        step3 = requests.post(web_server+'/video-call', json={
             'deviceName': deviceName1,
             'token': token1
             })
@@ -202,15 +229,135 @@ if __name__ == '__main__' :
     token2 = response.json()['token']
 
     """ MNCQUALIF-10996 """
-    Tests.MNCQUALIF_10966_in(deviceName2, email2, token2)
-    # if Tests.MNCQUALIF_10966_in(deviceName1,) :
-    #     sheet.append(("MNCQUALIF-10996 login", "OK"))
-    # else :
-    #     sheet.append(("MNCQUALIF-10996 login", "KO"))
 
+    if Tests.MNCQUALIF_10966_in(deviceName2, email2, token2) :
+        sheet.append(("MNCQUALIF-10996 login", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-10996 login", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
     if Tests.MNCQUALIF_10966_out(deviceName2, token2) :
         sheet.append(("MNCQUALIF-10996 logout", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
     else :
         sheet.append(("MNCQUALIF-10996 logout", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
 
+
+
+    Tests.MNCQUALIF_10966_in(deviceName1, email1, token1)
+    Tests.MNCQUALIF_10966_in(deviceName2, email2, token2)
+
+    time.sleep(2)
+    """ MNCQUALIF-10998 """
+
+    if Tests.MNCQUALIF_10998() :
+        sheet.append(("MNCQUALIF-10998", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-10998", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-10999 """
+
+    if Tests.MNCQUALIF_10999() :
+        sheet.append(("MNCQUALIF-10999", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-10999", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11000 """
+
+    if Tests.MNCQUALIF_11000() :
+        sheet.append(("MNCQUALIF-11000", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11000", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11001 """
+
+    if Tests.MNCQUALIF_11001() :
+        sheet.append(("MNCQUALIF-11001", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11001", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11009 """
+
+    if Tests.MNCQUALIF_11009() :
+        sheet.append(("MNCQUALIF-11009", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11009", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11005 """
+
+    if Tests.MNCQUALIF_11005() :
+        sheet.append(("MNCQUALIF-11005", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11005", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11005 """
+
+    if Tests.MNCQUALIF_11005() :
+        sheet.append(("MNCQUALIF-11005", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11005", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11004 """
+
+    if Tests.MNCQUALIF_11004_a() :
+        sheet.append(("MNCQUALIF-11004 one deletion", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11004 one deletion", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    if Tests.MNCQUALIF_11004_b() :
+        sheet.append(("MNCQUALIF-11004 all deletion", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11004 all deletion", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+    time.sleep(2)
+    """ MNCQUALIF-11011 """
+
+    if Tests.MNCQUALIF_11005() :
+        sheet.append(("MNCQUALIF-11011", "OK"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = greenFill
+    else :
+        sheet.append(("MNCQUALIF-11011", "KO"))
+        for cell in sheet[sheet.max_raw] :
+            cell.fill = redFill
+            
     wb.save(filename=filename)
